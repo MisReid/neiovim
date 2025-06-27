9df40d44997679c9c1310c10418f3352014a008b
Reid <rabeckes@mtu.edu>
2025-06-27 16:38:51 -0400

    The beningning

---
+++ lua/vim.lua
@@ -0,0 +1,21 @@
+vim.opt.number = true -- Show line numbers
+vim.opt.mouse = 'a' -- Enable mouse support
+vim.opt.showmode = false -- Don't show mode in command line
+vim.opt.undofile = true -- Save undo history
+vim.opt.ingorecase = true -- Ignore case in search
+vim.opt.smartcase = true -- Override ignorecase if search contains uppercase
+vim.signcolumn = 'yes' -- Always show sign column
+vim.opt.splitright = true -- Split windows to the right
+vim.opt.spitbelow = true -- Split windows below
+vim.opt.inccommand = 'split' -- Show live substitutions
+vim.opt.cursorline = true -- Highlight the current line
+vim.opt.scrolloff = 10 -- Keep # lines above and below the cursor
+
+-- TODO: Clipboard
+-- Stolen from kickstart. See ':help clipboard' to edit
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+
+-- Key mappings
+-- vim.keymap.set('n', '<C-n>', ':NvimTreeToggle<cr>', { silent = true, noremap = true })
