32875ac0c709656730a4aeae659ea95d9499e28e
Reid <rabeckes@mtu.edu>
2025-06-27 17:00:00 -0400

    Adding plugins

---
+++ lua/plugins/spec.lua
@@ -0,0 +1,29 @@
+return {
+  -- Plugins here!
+  {
+    'github/copilot.vim',
+  },
+  {
+    'nvim-tree/nvim-tree.lua',
+    version = '*',
+    lazy = false,
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      require('nvim-tree').setup {}
+    end,
+  },
+  {
+    'sainnhe/everforest',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.everforest_enable_italic = true
+      vim.g.everforest_background = 'soft'
+      vim.cmd.colorscheme 'everforest'
+    end,
+  },
+}
