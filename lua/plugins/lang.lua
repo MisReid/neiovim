6df4260bf34303d613e97a652c1f884cadb5d5e8
Reid <rabeckes@mtu.edu>
2025-06-28 17:36:15 -0400

    Changing copilot key

--- lua/plugins/lang.lua
+++ lua/plugins/lang.lua
@@ -6,7 +6,7 @@ return {
     event = 'InsertEnter',
     config = function()
       vim.g.copilot_no_tab_map = true
-      vim.keymap.set('i', '<C-Tab>', 'copilot#Accept("<CR>")', { expr = true, silent = true, replace_keycodes = false })
+      vim.keymap.set('i', '<S-Tab>', 'copilot#Accept("<CR>")', { expr = true, silent = true, replace_keycodes = false })
     end,
   },
   {
