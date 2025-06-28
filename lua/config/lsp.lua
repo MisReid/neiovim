55e1544bb70b0da6e4ac36802b60aef917bda6d1
Reid <rabeckes@mtu.edu>
2025-06-28 17:56:58 -0400

    Trying to get lsp to work

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -3,18 +3,18 @@
 return function()
   vim.api.nvim_create_autocmd("LspAttach", {
     group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
-    callback = function(args)
+    callback = function(event)
       local client_id = args.data.client_id
       local client = vim.lsp.get_client_by_id(client_id)
       local bufnr = args.buf
 
       -- LSP keymaps
-     vim.keymap.set("n", "gd", vim.lsp.buf.lsp_definitions, { buffer = bufnr("%"), desc = "[g]oto [d]efinition" }) 
-     vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = bufnr("%"), desc = "[g]oto [D]eclaration" })
-     vim.keymap.set("n", "gi", vim.lsp.buf.lsp_implementations, { buffer = bufnr("%"), desc = "[g]oto [i]mplementations" })
-     vim.keymap.set("n", "gt", vim.lsp.buf.lsp_type_definitions, { buffer = bufnr("%"), desc = "[g]oto [t]ype definition" })
-     vim.keymap.set("n", "cN", vim.lsp.buf.rename, { buffer = bufnr("%"), desc = "[c]hange [N]ame" }) 
-     vim.keymap.set("n", "<leader>ca", vim.lsp.code_action, { buffer = bufnr("%"), desc = "[c]ode [a]ction" })
+     vim.keymap.set("n", "gd", vim.lsp.buf.lsp_definitions, { buffer = event.buf, desc = "[g]oto [d]efinition" }) 
+     vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = event.buf, desc = "[g]oto [D]eclaration" })
+     vim.keymap.set("n", "gi", vim.lsp.buf.lsp_implementations, { buffer = event.buf, desc = "[g]oto [i]mplementations" })
+     vim.keymap.set("n", "gt", vim.lsp.buf.lsp_type_definitions, { buffer = event.buf, desc = "[g]oto [t]ype definition" })
+     vim.keymap.set("n", "cN", vim.lsp.buf.rename, { buffer = event.buf, desc = "[c]hange [N]ame" }) 
+     vim.keymap.set("n", "<leader>ca", vim.lsp.code_action, { buffer = event.buf, desc = "[c]ode [a]ction" })
 
       -- LSP autocommands
     end
@@ -27,6 +27,13 @@ return function()
       vim.lsp.buf.clear_references()
     end
   })
+
+  -- Stolen from kickstart.nvim
+  --  By default, Neovim doesn't support everything that is in the LSP specification.
+  --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
+  --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
+  capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
   require("mason-lspconfig").setup {
     ensure_installed = {
