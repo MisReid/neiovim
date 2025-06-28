260ba57d14055c39cc4bed69ac1fb0f0c3c31cca
Reid <rabeckes@mtu.edu>
2025-06-28 15:06:41 -0400

    Testing lsp

--- lua/plugins/lang.lua
+++ lua/plugins/lang.lua
@@ -4,4 +4,23 @@ return {
   {
     'github/copilot.vim',
   },
+  {
+    'folke/lazydev.nvim',
+    ft = 'lua',
+    opts = {
+      library = {
+        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+      },
+    },
+  },
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      { 'williamboman/mason.nvim', opts = {} }, -- Ensure mason is set up before lspconfig
+      'williamboman/mason-lspconfig.nvim',
+      { 'j-hui/fidget.nvim', opts = {} },
+      'hrsh7th/cmp-nvim-lsp',
+    },
+    config = require 'config.lsp',
+  },
 }
