be42399a590fdcd22c626dcb579e6d0a7a560c0f
Reid <rabeckes@mtu.edu>
2025-09-16 11:26:27 -0400

    Added which-key

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -130,5 +130,23 @@ return {
   },
   {
     'JefferyBoy/renamer.nvim',
-  }
+  },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
+  },
 }
